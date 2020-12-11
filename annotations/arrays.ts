b920d285031f9b37404efbf97748647ee7658b27
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 13:30:29 -0800

    add typed arrays

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -4,6 +4,17 @@ const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 //2d arrays
-
 // const carsByMake: string[][]
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(2);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});

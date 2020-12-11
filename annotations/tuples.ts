845d006f84fd413787c7078fd7417caf3e27af0e
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 13:56:41 -0800

    add tuple examples

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -11,6 +11,7 @@ const drink = {
 // pepsi[2] = 'brown';
 
 //Tuple not useful very often
+//CSV file - single row
 
 //create a type alias
 type Drink = [string, boolean, number];
@@ -21,3 +22,12 @@ const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
 
 // pepsi[0] = 40;
+
+//we can't understand what the numbers are
+const carSpecs: [number, number] = [400, 3354];
+
+//its clearer at one glance to understand what these values mean in this model
+const carStats: { horsepower: number; weight: number } = {
+  horsepower: 400,
+  weight: 3354,
+};

a5de6d9b39bf33f7b5116dea0e8ef1c2774123e9
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 13:00:58 -0800

    add annotation for two functions and notes

---
+++ annotations/functions.ts
@@ -0,0 +1,14 @@
+const add = (a: number, b: number):number => {
+  return a + b;
+  };
+
+//inference
+// const add = (a: number, b: number) => {
+// return a + b;
+// };
+
+
+//Always add on a return type e.g.    :number
+const subtract = (a: number, b:number) => {
+  a - b;
+};

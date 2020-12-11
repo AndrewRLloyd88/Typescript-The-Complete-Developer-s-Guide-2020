53a5e61d4559b70209a95eb06378a86b45e12324
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 13:26:36 -0800

    add arrays in typescript

---
+++ annotations/arrays.ts
@@ -0,0 +1,9 @@
+// const carMakers: string[] = [];
+
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+//2d arrays
+
+// const carsByMake: string[][]
+const carsByMake = [['f150'], ['corolla'], ['camaro']];

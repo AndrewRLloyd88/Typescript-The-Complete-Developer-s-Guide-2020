a6e6fa6734f49e8369deaf3b57453ad124d941db
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 14:18:26 -0800

    add drink object and pass to printSummary our new more generic function

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -14,6 +14,15 @@ const oldCivic = {
   },
 };
 
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
 const printSummary = (item: Reportable): void => {
   // console.log(`Name: ${vehicle.name}`);
   // console.log(`Year: ${vehicle.year}`);
@@ -22,3 +31,4 @@ const printSummary = (item: Reportable): void => {
 };
 
 printSummary(oldCivic);
+printSummary(drink);

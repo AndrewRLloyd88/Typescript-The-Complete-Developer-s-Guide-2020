7f10bb069443ad65f5f566045705a7945f4e51e9
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-11 13:07:50 -0800

    add function return annotation types for never and void

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -20,3 +20,32 @@ function divide(a:number, b:number):number {
 const multiply = function(a: number, b: number):number {
   return a*b;
 }
+
+//no return value from this function
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+//never === we will never reach the end of this function
+const throwError = (message: string): never => {
+  throw new Error(message)
+
+  //more common to have a return value
+}
+
+//this is a more common pattern
+const throwError2 = (message: string): string => {
+  if(!message){
+  throw new Error(message)
+  }
+
+  return(message)
+  //more common to have a return value
+}
+
+//this is also more common pattern - no return
+const throwError3 = (message: string): void => {
+  if(!message){
+  throw new Error(message)
+  }
+}

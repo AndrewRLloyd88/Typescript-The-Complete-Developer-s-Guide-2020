ce32df082fd6d2123cf2ac28c6bca30cd6cc1227
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-05 14:08:38 -0800

    cover class modifiers

--- classes.ts
+++ classes.ts
@@ -1,25 +1,33 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-  honk(): void {
+  protected honk(): void {
     console.log('Honk Honk');
   }
 }
 //vehicle is the Super or parent class
 const vehicle = new Vehicle();
-
-vehicle.drive();
 vehicle.honk();
 
 //overridden a method on the child class
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('Vroooom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
 
-car.drive();
+car.startDrivingProcess();
 car.honk();
+
+//modifiers public private and protected
+//restict access to different functions or variables
+//by default Public modifier is added to properties
+//Public - method can be called anywhere anytime
+//Private - can only be called by other methds in this class
+//Private - we are not adding in any layer of app security just stopping other developers calling methods
+//Protected means the method can only be called by subclasses of the parent or super class.

1bb80a41798d721819ac13bb3762fd7fa1d51bdb
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-05 14:00:15 -0800

    cover super and child class method override

--- classes.ts
+++ classes.ts
@@ -6,8 +6,20 @@ class Vehicle {
     console.log('Honk Honk');
   }
 }
-
+//vehicle is the Super or parent class
 const vehicle = new Vehicle();
 
 vehicle.drive();
 vehicle.honk();
+
+//overridden a method on the child class
+class Car extends Vehicle {
+  drive(): void {
+    console.log('Vroooom');
+  }
+}
+
+const car = new Car();
+
+car.drive();
+car.honk();

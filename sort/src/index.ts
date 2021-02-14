b3041721dca3ef961146d79fe640198feec972aa
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 15:31:14 -0800

    scaffold sorter class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,9 @@
-console.log('one two three');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('something');
-};
+  sort(): void {}
+}
 
-logSomething();
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);

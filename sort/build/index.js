63efaf992e741871630b2d49b1bd5b164d500ad1
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 17:12:32 -0800

    implement methods in charactersCollection to use sorting algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 //numbersCollection satisfies Sortable in Sorter.ts
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('XaayzEb');
+var charSorter = new Sorter_1.Sorter(charactersCollection);
+charSorter.sort();
+console.log(charactersCollection.data);

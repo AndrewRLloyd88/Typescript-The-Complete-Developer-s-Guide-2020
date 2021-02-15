05f67250d2a97361c85e01a883f182988f5fef85
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 16:00:14 -0800

    add type guard to instanceof arrays

--- sort/build/index.js
+++ sort/build/index.js
@@ -8,10 +8,27 @@ var Sorter = /** @class */ (function () {
         //bubble sorting algorithm
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                //bad approach
+                //all of this only works if collection is number[]
+                //if collection is an array of numbers
+                //type guards
+                //instanceof constructor function e.g. instanceof Sorter, instanceof Date
+                if (this.collection instanceof Array) {
+                    //typescript understands that collection must be an array
+                    //it has all the properties and features associated with array
+                    //union restriction is lifted
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                //only going to work if collection is string
+                //if collection is a string do this logic instead
+                //~~logic to compare and swap characters in a string
+                //type guard in typescript does not work with typeof array
+                if (typeof this.collection === 'string') {
+                    //restored access to properties that are available to string
                 }
             }
         }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,5 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
@@ -7,10 +7,29 @@ class Sorter {
     //bubble sorting algorithm
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        //bad approach
+
+        //all of this only works if collection is number[]
+        //if collection is an array of numbers
+        //type guards
+        //instanceof constructor function e.g. instanceof Sorter, instanceof Date
+        if (this.collection instanceof Array) {
+          //typescript understands that collection must be an array
+          //it has all the properties and features associated with array
+          //union restriction is lifted
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+
+        //only going to work if collection is string
+        //if collection is a string do this logic instead
+        //~~logic to compare and swap characters in a string
+        //type guard in typescript does not work with typeof array
+        if (typeof this.collection === 'string') {
+          //restored access to properties that are available to string
         }
       }
     }

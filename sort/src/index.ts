b6a12e569e8590d18a9921b97abfde996ada5f47
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 17:01:54 -0800

    define Sortable interface that includes a length a compare function and swap function

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,6 +2,7 @@ import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+//numbersCollection satisfies Sortable in Sorter.ts
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);

a5a34725825f8a6ac6853f564767a9cfc0325469
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 16:52:54 -0800

    refactor numbersCollection to get the length, compare the two numbers and swap two numbers

--- sort/build/Sorter.js
+++ sort/build/Sorter.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 var Sorter = /** @class */ (function () {
-    function Sorter(collection /*TODO fix me*/) {
+    function Sorter(collection) {
         this.collection = collection;
     }
     Sorter.prototype.sort = function () {
@@ -10,10 +10,8 @@ var Sorter = /** @class */ (function () {
         //bubble sorting algorithm
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
                 }
             }
         }
---
+++ sort/src/NumbersCollection.ts
@@ -0,0 +1,20 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  //keyword get means you do not have to call length as a function
+  get length(): number {
+    return this.data.length;
+  }
+
+  //swap two elements in a pair with bubblesort
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  //swapping logic specifically for an array of numbers
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+}

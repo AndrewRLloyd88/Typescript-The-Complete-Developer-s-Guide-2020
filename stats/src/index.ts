5a18ebcdf183513da0cd356c8104f922e308abd2
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-20 15:55:05 -0800

    use static method in MatchReader to return a preconfigured instance of MatchReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './ReportTargets/ConsoleReport';
-import { HtmlReport } from './ReportTargets/HtmlReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
-//create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-//Create an instance of MatchReader and pass in something satisfying the 'DataReader' Interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-//.load allows us to reference matchReader.matches
-
-//object composition - main object delegates out behaviors to other objects
-
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);

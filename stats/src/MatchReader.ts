2a82a1c4b1243eed9cec956aa8e3d43d68f66c4b
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-20 14:24:45 -0800

    refactpr MatchReader to take in a tuple of matches and use the map method on this.reader.data

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -10,19 +10,25 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
 
-  // .map(
-  //   (row: string[]): MatchData => {
-  //     return [
-  //       dateStringToDate(row[0]),
-  //       row[1],
-  //       row[2],
-  //       parseInt(row[3]),
-  //       parseInt(row[4]),
-  //       row[5] as MatchResult, //"H", "A" or "D"
-  //       row[6],
-  //     ];
-  //   }
-  // );
+  //take a look at the provided reader
+  load(): void {
+    this.reader.read();
+    //transform the strings
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, //"H", "A" or "D"
+          row[6],
+        ];
+      }
+    );
+  }
 }

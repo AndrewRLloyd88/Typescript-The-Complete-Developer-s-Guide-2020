59abc88197019a70e29928742352eff519224394
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-20 15:34:26 -0800

    generate html report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,7 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './ReportTargets/ConsoleReport';
+import { HtmlReport } from './ReportTargets/HtmlReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
@@ -14,9 +15,6 @@ matchReader.load();
 
 //object composition - main object delegates out behaviors to other objects
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);

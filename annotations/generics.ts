c1b5ae76be0a93839443e9d24e024adadff20cda
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-20 17:18:12 -0800

    write generic class for ArrayOfAnything

---
+++ annotations/generics.ts
@@ -0,0 +1,26 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+//T is just an argument for TypeOfData
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(['a', 'b', 'c']);

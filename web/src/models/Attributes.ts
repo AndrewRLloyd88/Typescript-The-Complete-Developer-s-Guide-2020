566579a34375331527e7a66bbc4f7d52386cafd8
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-05 14:37:31 -0800

    set up a constraint that uses the keys of userProps for attributes

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -1,11 +1,28 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  //Method type annotation <K extends keyof T> = generic constraint
+  //(key: K) limits types K can be e.g. one of the keys of T
+  //lookup the value of T(UserProps) at the index of K
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+//the keys of an object can be a type as well
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: 'whatever',
+});
+
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');

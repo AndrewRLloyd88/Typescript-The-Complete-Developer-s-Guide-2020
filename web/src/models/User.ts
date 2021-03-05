20d6291caa85ff71c9968043204e4f032468a8a6
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-05 14:16:23 -0800

    extracting attributes class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -13,16 +13,4 @@ export class User {
   //we will always get a copy of eventing - we will probably never swap out eventing
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
-
-//assign some properties to user upon creation

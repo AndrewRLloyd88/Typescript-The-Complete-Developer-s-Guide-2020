4ed41b465b36a66823d6583974c34ba3945c348f
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-07 15:32:27 -0800

    add all methods of User into Model

--- web/src/models/Model.ts
+++ web/src/models/Model.ts
@@ -1,4 +1,6 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise, AxiosResponse } from 'axios';
+
+type Callback = () => void;
 
 interface ModelAttributes<T> {
   set(value: T): void;
@@ -11,11 +13,65 @@ interface Sync<T> {
   save(data: T): AxiosPromise;
 }
 
-type Callback = () => void;
-
 interface Events {
   on: (eventName: string, callback: Callback) => void;
   trigger: (eventName: string) => void;
 }
 
-export class Model {}
+interface HasId {
+  id?: number;
+}
+
+//pass in a type T that specifies the data structure
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  //getter returning a reference of the on function not calling it
+  //paranthesis here invoke the on method on Eventing not on right here!
+  //passthrough methods
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      //we always want to trigger a change in this class when changing data
+      this.set(response.data);
+    });
+  }
+
+  //any time we call save we get all the properties and save it
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse) => {
+        //any time we save successfully we call the save callback
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { Model } from './Model';
 
 export interface UserProps {
   id?: number;
@@ -11,59 +8,4 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  //we will always get a copy of eventing - we will probably never swap out eventing
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  //initializing submodule attributes
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  //getter returning a reference of the on function not calling it
-  //paranthesis here invoke the on method on Eventing not on right here!
-  //passthrough methods
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      //we always want to trigger a change in this class when changing data
-      this.set(response.data);
-    });
-  }
-
-  //any time we call save we get all the properties and save it
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse) => {
-        //any time we save successfully we call the save callback
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
-  }
-}
+export class User extends Model<UserProps> {}

98dd2c9a1f02859b8d543c67856de67fc1f044a1
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-07 17:18:36 -0800

    set up events map

--- web/src/Views/UserForm.ts
+++ web/src/Views/UserForm.ts
@@ -1,11 +1,23 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  //relate different events to watch for to what events we want to run
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi There');
+  }
+
   template(): string {
     return `
     <div>
     <h1>User Form</h1>
     <input></input>
+    <button>Click me</button>
     </div>
     `;
   }

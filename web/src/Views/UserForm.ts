6754e93cc23c9a6d280cda7685a3cb3d0833af46
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-07 18:12:12 -0800

    add tsconfig.json and refactor code to adhere to strictNullChecks

--- web/src/Views/UserForm.ts
+++ web/src/Views/UserForm.ts
@@ -16,11 +16,21 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.update-name': this.onUpdateNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onUpdateNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    //adding a type guard to adhere to strict null checks
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
   };
 
   template(): string {
@@ -30,7 +40,7 @@ export class UserForm {
     <div>User name: ${this.model.get('name')}</div>
     <div>User age: ${this.model.get('age')}</div>
     <input></input>
-    <button>Click me</button>
+    <button class="update-name">Update Name</button>
     <button class="set-age">Set Random Age</button>
     </div>
     `;
--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,10 @@ import { User } from './models/User';
 import { UserForm } from './Views/UserForm';
 
 const user = User.buildUser({ name: 'NAME', age: 24 });
-const userForm = new UserForm(document.getElementById('root'), user);
-userForm.render();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root Element not found');
+}

495b1b6769b08146f9622269c7b5159c019be17e
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-07 17:12:23 -0800

    write class to display HTML in views

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './models/User';
+import { UserForm } from './Views/UserForm';
 
 const collection = User.buildUserCollection();
+const userForm = new UserForm(document.getElementById('root'));
+userForm.render();
 
 collection.on('change', () => {
   console.log(collection);

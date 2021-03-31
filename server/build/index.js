1a3b0dacd5ac5fb8f23081debabecfb9cb6ae535
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-31 16:44:50 -0700

    add initial code to server

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send("\n  <div><h1>Hi There</h1></div>");
+});
+app.listen(1793, function () {
+    console.log('listening on port 1793');
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi there')
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+  <div><h1>Hi There</h1></div>`)
+})
+
+app.listen(1793, () => {
+  console.log('listening on port 1793')
+})

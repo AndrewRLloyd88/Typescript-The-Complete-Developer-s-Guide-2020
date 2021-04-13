fe68dc40b87a542cd4c250bfcd5f035305db63b4
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-12 23:11:48 -0700

    wrap decorator in another function creating decorator factory returning our decorator function

--- annotations/decorators.ts
+++ annotations/decorators.ts
@@ -6,21 +6,23 @@ class Boat {
   }
 
   //log error will automatically catch errors
-  @logError
+  @logError('Oops boat was sunk in ocean!')
   pilot(): void {
     throw new Error()
     console.log('swish')
   }
 }
 
-//decorator
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value
-  desc.value = function () {
-    try {
-      method()
-    } catch (e) {
-      console.log('oops, boat was sunk')
+//decorator factory that returns our decorator function
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value
+    desc.value = function () {
+      try {
+        method()
+      } catch (e) {
+        console.log(errorMessage)
+      }
     }
   }
 }

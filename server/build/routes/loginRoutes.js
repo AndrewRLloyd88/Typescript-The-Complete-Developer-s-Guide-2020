0f818c8ea4dd136ad54dac76f7e0e48d143f0dbc
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-12 22:20:13 -0700

    finish off basic express routing with protected route

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+var requireAuth = function (req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not Permitted');
+};
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -34,4 +42,7 @@ router.get('/', function (req, res) {
 router.get('/logout', function (req, res) {
     req.session = undefined;
     res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to protected route, Logged in User!');
 });

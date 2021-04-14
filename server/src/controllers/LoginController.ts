a1565308449bda0c64c9804c22d8f4ee8bd496cb
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-13 20:22:00 -0700

    removeloginRoutes and wire everything up via RootController and LoginController

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -37,4 +37,10 @@ class LoginController {
       res.send('Invalid Email or Password')
     }
   }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = undefined
+    res.redirect('/')
+  }
 }
